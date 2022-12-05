1b4d4696d2ae33393f4eacccc6143a3cc6b1e4de
ohspc89 <jinseok.oh@pm.me>
2022-12-04 21:58:24 -0800

    Testing multiple windows

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 from PyQt6.QtWidgets import (QMessageBox, QMainWindow, QApplication, QComboBox,
         QLabel, QWidget, QToolBar, QStatusBar, QDialog, QVBoxLayout, QGridLayout,
-        QFileDialog)
+        QFileDialog, QPushButton)
 
 from PyQt6.QtGui import QAction, QIcon
 from PyQt6.QtCore import Qt, QSize
@@ -16,38 +16,87 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        # Title of the main window
+        self.setWindowTitle("Sensor Data Analysis App")
         self.initUI()
 
-    # A different way of organizing an app.
+    def initUI(self):
+        l = QVBoxLayout()       # Box layout
+        button = QPushButton("Extract times sensors were worn")
+        button.clicked.connect(self.show_redcap_window)
+        l.addWidget(button)     # add the button to the widget
+
+        button2 = QPushButton("Preprocess h5 file(s)")
+        button2.clicked.connect(self.show_preprocess_window)
+        l.addWidget(button2)
+
+        widget = QWidget()
+        widget.setLayout(l)
+        self.setCentralWidget(widget)
+
+        #self.setStatusBar(QStatusBar(self))
+
+        self.w = None       # This is to check if the new window has been opened
+        self.w2 = None
+
+        self.show()
+
+    def show_redcap_window(self, checked):
+        if self.w is None:
+            self.w = ConvertWindow(self)
+        self.w.show()
+
+    def show_preprocess_window(self, checked):
+        if hasattr(self, 'out'):
+            print('out exists')
+        else:
+            print('redcap not yet processed')
+        
+
+# This is the window that will handle the conversion of a REDCap data export
+# to whatever csv file we want for further preprocessing
+class ConvertWindow(QMainWindow):
+
+    def __init__(self, parent):
+        super(ConvertWindow, self).__init__(parent)
+        self.initUI()
+        self.parent = parent
+        print(self.parent)
+
     def initUI(self):
 
-        self.setWindowTitle("Redcap Export App")
+        self.setWindowTitle("Extract times from Redcap")
 
         layout = QGridLayout()
+        #self.setLayout(layout)
 
-        layout.addWidget(QLabel('Select corresponding columns\nfrom your csv file\nfor each item.'), 0, 0)
+        layout.addWidget(QLabel('Select corresponding columns\nfrom your csv file for each item.'), 1, 0)
 
         self.id_dropdown = QComboBox()
         self.visit_dropdown = QComboBox()
         self.donned_dropdown = QComboBox()
         self.doffed_dropdown = QComboBox()
 
-        layout.addWidget(QLabel('id : '),          1, 0)
-        layout.addWidget(self.id_dropdown,         1, 1)
-        layout.addWidget(QLabel('visit number: '), 2, 0)
-        layout.addWidget(self.visit_dropdown,      2, 1)
-        layout.addWidget(QLabel('time donned: '),  3, 0)
-        layout.addWidget(self.donned_dropdown,     3, 1)
-        layout.addWidget(QLabel('time doffed: '),  4, 0)
-        layout.addWidget(self.doffed_dropdown,     4, 1)
+        layout.addWidget(QLabel('id : '),          2, 0)
+        layout.addWidget(self.id_dropdown,         2, 1)
+        layout.addWidget(QLabel('visit number: '), 3, 0)
+        layout.addWidget(self.visit_dropdown,      3, 1)
+        layout.addWidget(QLabel('time donned: '),  4, 0)
+        layout.addWidget(self.donned_dropdown,     4, 1)
+        layout.addWidget(QLabel('time doffed: '),  5, 0)
+        layout.addWidget(self.doffed_dropdown,     5, 1)
 
         widget = QWidget()
-        widget.setLayout(layout)
+        widget.setLayout(layout)            # so I think you should have a widget to place a layout
+        self.setCentralWidget(widget)       # and in a window you centre the widget
 
-        self.setCentralWidget(widget)
+        # setting a status bar
+        self.setStatusBar(QStatusBar(self))
 
+        # setting a toolbar - see that this is located at the top (0,0) of the grid layout
         toolbar = QToolBar("My main toolbar")
         toolbar.setIconSize(QSize(16,16))
+        #layout.addWidget(toolbar, 0, 0)
         self.addToolBar(toolbar)
 
         # Icon to 'load' the file
@@ -62,9 +111,6 @@ class MainWindow(QMainWindow):
         convert_action.triggered.connect(self.file_convert)
         toolbar.addAction(convert_action)
 
-        # setting a status bar
-        self.setStatusBar(QStatusBar(self))
-
         # It seems like Mac forces the menu bar to appear at the top, no matter what
         # So switching to a button
         #menu = self.menuBar()
@@ -72,7 +118,7 @@ class MainWindow(QMainWindow):
         #file_menu.addAction(load_action)
         #file_menu.addSeparator()
 
-        self.show()
+        #self.show()
 
     def open_filediag(self, s):
         # You will be opening a file dialogue to search for the file
@@ -83,7 +129,6 @@ class MainWindow(QMainWindow):
         if self.fileNames[0]:
             self.dt = pd.read_csv(self.fileNames[0])
             cols = self.dt.columns
-
             # Then fill in the dropdown menus with the column names of the original csv file
             self.id_dropdown.addItems(cols)
             self.visit_dropdown.addItems(cols)
@@ -94,8 +139,8 @@ class MainWindow(QMainWindow):
             
     def file_convert(self):
         if hasattr(self, 'dt'):
-            # Columns Of InterestS
-            # Currently, the order should be [id, visit number, time donned, time dofed]
+            # Columns Of InterestS (cois)
+            # Currently, the order should be [id, visit number, time donned, time doffed]
             cois = [self.id_dropdown.currentText(), self.visit_dropdown.currentText(),
                     self.donned_dropdown.currentText(), self.doffed_dropdown.currentText()]
             # Get the subset
@@ -108,8 +153,10 @@ class MainWindow(QMainWindow):
             if (outname[0] == ''):
                 pass
             else:
+                # make the MainWindow to have this output
+                self.parent.out = out
                 out.to_csv(outname[0], index=False)
-            
+
                 # If things went well, throw out a message
                 msg = QMessageBox(self)
                 msg.setWindowTitle("Important Message")
